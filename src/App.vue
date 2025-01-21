28cad721a14378d9008a8682b3ad0c47005ebb5d
Sonia <sonia.blankenmeister@gmail.com>
2025-01-21 17:08:34 +0100

    feature: add Navbar component in app.vue

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,24 @@
 
 <template>
 
-  <nav>
-    <RouterLink to="/">Go to Home</RouterLink>
+  <!--<nav>
+    <RouterLink to="/">Evenment-Hall</RouterLink>
     <RouterLink to="/test">Go to Test</RouterLink>
-  </nav>
+  </nav>-->
+
+  <Navbar />
+
+
   <main>
     <RouterView />
   </main>
 </template>
+
+
+<script setup>
+import Navbar from './components/Navbar.vue';
+</script>
+
+<style>
+/* Styles globaux ici */
+</style>
